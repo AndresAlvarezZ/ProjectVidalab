087b771d2728873461df0372fb1793cdfadf8aad
Furia10-A <aias1@outlook.es>
2020-09-12 14:55:07 -0600

    cambio del nombre del boton

--- resources/views/nuevoAdministrador.blade.php
+++ resources/views/nuevoAdministrador.blade.php
@@ -132,7 +132,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Registrar Administrador') }}
+                                        {{ __('Registrar') }}
                                     </button>
 
                                 </div>
