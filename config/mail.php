c9cb71a78e6c496f1c28480d874fff3cabe7c073
Furia10-A <aias1@outlook.es>
2020-09-13 20:20:08 -0600

    cambios en el reset password y notificaciones en español

--- config/mail.php
+++ config/mail.php
@@ -37,7 +37,7 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 465),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
@@ -84,7 +84,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'myusername@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'andress.170395@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'hawle'),
     ],
 
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Tu contraseña ha sido restablecida!',
+    'sent' => 'Hemos enviado un link al correo para restablecer tu contraseña!',
+    'throttled' => 'Espera unos segundos para reintentar',
+    'token' => 'This password reset token is invalid. El token de restablecimiento de contraseña es inválido',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico",
 
 ];
