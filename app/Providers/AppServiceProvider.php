287cda19d71abb24a6b48b38c02617a5c5304e67
Furia10-A <70809214+Furia10-A@users.noreply.github.com>
2020-10-31 19:23:42 -0600

    prueba3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use WindowsAzure\Common\ServicesBuilder;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        
+
 
     }
 }
