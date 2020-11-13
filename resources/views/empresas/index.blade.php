88b5edc16fc4cf53b4338d66beb7ba154f5ef154
Iryeri <70235174+Iryeri@users.noreply.github.com>
2020-11-13 10:47:07 -0600

    Cambio de título

--- resources/views/empresas/index.blade.php
+++ resources/views/empresas/index.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-40">
             <div class="card">
-                <div class="card-header"><h4><b><center>Registro de todas las empresas</center></b></h4></div>
+                <div class="card-header"><h4><b><center>Registro de todas las Empresas</center></b></h4></div>
                     <div class="card-body">
                         <ul class="list-group">
                             <table class="table">
