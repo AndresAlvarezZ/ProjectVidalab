ed3732ca5c7b727a90c0ba8c38dc5ffa5b721f85
Furia10-A <70809214+Furia10-A@users.noreply.github.com>
2020-12-13 10:30:12 -0600

    corrección de imagen en archivo multimedia

--- resources/views/archivos/multimediaCliente.blade.php
+++ resources/views/archivos/multimediaCliente.blade.php
@@ -16,12 +16,12 @@
         @foreach($archivos as $archivo)
             <div class="card">
               <?php if ($archivo->tipoDeArchivo=='1'): ?>
-                <img id="imagenMultimedia" src="/imagenes/{{$archivo->nombreDelArchivo}}" alt="contenido multimedia" control>
+                <img id="imagenMultimedia" src="/archivosMultimedia/{{$archivo->nombreDelArchivo}}" alt="contenido multimedia" control>
               <?php endif; ?>
                 <?php if ($archivo->tipoDeArchivo=='2'): ?>
                   <video width="320" height="240" controls>
-                          <source src="imagenes/{{$archivo->nombreDelArchivo}}" type="video/mp4">
-                          <source src="imagenes/{{$archivo->nombreDelArchivo}}" type="video/ogg">
+                          <source src="archivosMultimedia/{{$archivo->nombreDelArchivo}}" type="video/mp4">
+                          <source src="archivosMultimedia/{{$archivo->nombreDelArchivo}}" type="video/ogg">
                           Your browser does not support the video tag.
                         </video>
                 <?php endif; ?>
