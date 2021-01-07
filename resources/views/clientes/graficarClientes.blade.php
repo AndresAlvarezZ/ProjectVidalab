2a33e65f865bcc5741805acfbd22d106ed32fb1e
Iryeri <70235174+Iryeri@users.noreply.github.com>
2021-01-06 18:24:08 -0600

    Estilo a la gráfica

--- resources/views/clientes/graficarClientes.blade.php
+++ resources/views/clientes/graficarClientes.blade.php
@@ -1,11 +1,16 @@
-@extends('layouts.especial')
-@section('content')
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
     <head>
-        <link href="{{ asset('css/menuAdministrador.css') }}?v=<?php echo(rand()); ?>" rel="stylesheet">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     </head>
-    
+
     <body>
+        <p>
+            <br></br>
+            <a href="/homeAdmin" class="btn btn-info " data-toggle="tooltip" data-placement="right" title="Click para ir al menú principal"><span class="icon-home"> </span> Volver al Menú Principal</a>
+        </p>
+        
         <div id="container"></div>
     </body>
 
@@ -18,6 +23,81 @@
 
     <script type="text/javascript">
         var clientes = <?php echo json_encode($datos)?>;
+        Highcharts.setOptions
+        ({
+            chart: 
+            {
+                style: 
+                {
+                    fontFamily: 'monospace',
+                    color: "rgb(223, 89, 0)"
+                }
+            },
+            title: 
+            {
+                style: 
+                {
+                    color: 'rgb(223, 89, 0)',
+                    font: 'bold 20px "Trebuchet MS", Verdana, sans-serif'
+                }
+            },
+            subtitle: 
+            {
+                style: 
+                {
+                    color: '#119281',
+                    font: 'bold 14px "Trebuchet MS", Verdana, sans-serif'
+                }
+            },
+            xAxis: 
+            {
+                lineColor: '#000',
+                tickColor: '#000',
+                labels: 
+                {
+                    style: 
+                    {
+                        color: 'rgb(223, 89, 0)',
+                        font: '12px Trebuchet MS, Verdana, sans-serif'
+                    }
+                },
+                title: 
+                {
+                    style: 
+                    {
+                        color: '#333',
+                        fontWeight: 'bold',
+                        fontSize: '12px',
+                        fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+                    }            
+                }
+                },
+            yAxis: 
+            {
+                minorTickInterval: 'auto',
+                lineColor: '#000',
+                lineWidth: 1,
+                labels: 
+                {
+                    style: 
+                    {
+                        color: 'rgb(223, 89, 0)',
+                        font: '12px Trebuchet MS, Verdana, sans-serif'
+                    }
+                },
+                title: 
+                {
+                    style: 
+                    {
+                        color: '#333',
+                        fontWeight: 'bold',
+                        fontSize: '12px',
+                        fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+                    }            
+                }
+            },
+        });
+
         Highcharts.chart('container', 
         {
             lang: 
@@ -36,7 +116,7 @@
             },
             title:
             {
-                text: 'Clientes registrados en HUMAcheck',
+                text: 'Clientes Registrados en HUMAcheck',
             },
             subtitle:
             {
@@ -110,4 +190,4 @@
             }
         });
     </script>
-@endsection
+</html>
